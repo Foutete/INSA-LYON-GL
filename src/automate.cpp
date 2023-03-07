428f156c8c58af361b1d67743fe48516e244953a
Foutete <foltetefrancois@gmail.com>
2023-03-07 16:32:02 +0100

    Transation

--- src/automate.cpp
+++ src/automate.cpp
@@ -1,33 +1,28 @@
 #include "automate.h"
 
+#include "state.h"
 
-Automate::Automate(Lexer * lexer) {
-    lexer = lexer;
-}
+Automate::Automate(Lexer* lexer) { lexer = lexer; }
 
 void Automate::decalage(Symbole* s, State* state) {
-    pileEtats.push_back(state);
-    pileSymboles.push_back(s);
-    if (s->isTerminal()) {
-        lexer->Avancer();
-    }
+  pileEtats.push_back(state);
+  pileSymboles.push_back(s);
+  if (s->isTerminal()) {
+    lexer->Avancer();
+  }
 }
 
 void Automate::reduction(Symbole* s, int n) {
-
-    for (int i = 0; i < n; i++) {
-        delete(pileEtats.back());
-        pileEtats.pop_back();
-    }
-    pileEtats.back(); // TODO ->transition(*this,s);
+  for (int i = 0; i < n; i++) {
+    delete (pileEtats.back());
+    pileEtats.pop_back();
+  }
+  pileEtats.back()->transition(*this, s);  // TODO ->transition(*this,s);
 }
 
-void Automate::run() {
-
-}
+void Automate::run() {}
 
 void Automate::popAndDestroySymbol() {
-    delete(pileSymboles.back());
-    pileSymboles.pop_back();   
+  delete (pileSymboles.back());
+  pileSymboles.pop_back();
 }
-
